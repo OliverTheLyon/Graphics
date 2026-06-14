b6b79a678376706ab03257a3ce5324229363c695
oliver lyon <oll724@mail.usask.ca>
2026-06-13 19:22:29 -0600

    added entity classes with stubs

---
+++ Utility/entity.h
@@ -0,0 +1,13 @@
+//
+// Created by oliverlyon on 2026-06-13.
+//
+
+#ifndef GAME_ENTITY_H
+#define GAME_ENTITY_H
+
+
+class entity {
+};
+
+
+#endif //GAME_ENTITY_H
---
+++ Utility/moveable_entity.cpp
@@ -0,0 +1,5 @@
+//
+// Created by oliverlyon on 2026-06-13.
+//
+
+#include "entity.h"
