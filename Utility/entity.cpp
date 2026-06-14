a54a3f45acb77efdaf8bce35045bfdbba192aa67
oliver lyon <oll724@mail.usask.ca>
2026-06-14 12:07:20 -0600

    added entity code, started moving code over to new namespace

--- Utility/entity.cpp
+++ Utility/entity.cpp
@@ -1,5 +1,39 @@
-//
-// Created by oliverlyon on 2026-06-13.
-//
-
 #include "entity.h"
+
+namespace OKengine {
+
+    entity::entity() {
+        curr_pos = glm::vec3(0.0f); //defult location
+    }
+
+    entity::entity(glm::vec3 position) {
+        curr_pos = position;
+    }
+
+    entity::entity(std::shared_ptr<mesh> new_mesh) {
+        curr_pos = glm::vec3(0.0f); //defult location
+        mesh = new_mesh;
+    }
+
+    entity::entity(glm::vec3 position, std::shared_ptr<mesh> new_mesh) {
+        curr_pos = position;
+        mesh = new_mesh;
+    }
+
+    void entity::set_curr_position(glm::vec3 position) {
+        curr_pos = position;
+    }
+
+    void entity::set_mesh(std::shared_ptr<mesh> new_mesh) {
+        mesh = new_mesh;
+    }
+
+    glm::vec3* entity::get_curr_position() {
+        return &curr_pos;
+    }
+
+    std::weak_ptr<mesh> entity::get_mesh() {
+        //TODO CHECK MESH EXISTS!
+        return mesh;
+    }
+}
--- Utility/entity.h
+++ Utility/entity.h
@@ -5,51 +5,64 @@
 #include <glm/glm.hpp>
 
 
-class entity {
+namespace OKengine {
+    class entity {
 
-private:
-    std::shared_ptr<mesh> mesh; //MESH
-    glm::vec3 curr_pos;         //current centroid position
+        private:
+        std::shared_ptr<mesh> mesh; //MESH
+        glm::vec3 curr_pos;         //current centroid position
 
-public:
-    /**
-     * generic constructor
-     */
-    entity();
+        public:
+        /**
+         * generic constructor
+         */
+        entity();
 
-    /**
-     *
-     * @param new_mesh the entities mesh
-     */
-    entity(std::shared_ptr<mesh> new_mesh);
+        /**
+         *
+         * @param pos the entities mesh
+         */
+        entity(glm::vec3 position);
 
-    /**
-     * placeholder for if entities have any memory ownership
-     */
-    ~entity(){};
+        /**
+         *
+         * @param new_mesh the entities mesh
+         */
+        entity(std::shared_ptr<mesh> new_mesh);
 
-    /**
-     *
-     * @param position (x,y,z) float vector for the location of the centroid
-     */
-    void set_curr_position(glm::vec3 position);
+        /**
+         *
+         * @param new_mesh the entities mesh
+         */
+        entity(glm::vec3 position, std::shared_ptr<mesh> new_mesh);
 
-    /**
-     *
-     * @param new_mesh new mesh pointer to replace the old one
-     */
-    void set_mesh(std::shared_ptr<mesh> new_mesh);
+        /**
+         * placeholder for if entities have any memory ownership
+         */
+        ~entity(){};
 
-    /**
-     *
-     * @return returns a pointer to the 3 float vector holding the current position
-     */
-    glm::vec3* get_curr_position();
+        /**
+         *
+         * @param position (x,y,z) float vector for the location of the centroid
+         */
+        void set_curr_position(glm::vec3 position);
 
-    /**
-     * TODO: THROW EXCEPTION when no mesh in entity
-     * @return weak copy pointer to the mesh inside the entity
-     */
-    std::weak_ptr<mesh> get_mesh();
-};
+        /**
+         *
+         * @param new_mesh new mesh pointer to replace the old one
+         */
+        void set_mesh(std::shared_ptr<mesh> new_mesh);
 
+        /**
+         *
+         * @return returns a pointer to the 3 float vector holding the current position
+         */
+        glm::vec3* get_curr_position();
+
+        /**
+         * TODO: THROW EXCEPTION when no mesh in entity
+         * @return weak copy pointer to the mesh inside the entity
+         */
+        std::weak_ptr<mesh> get_mesh();
+    };
+}
