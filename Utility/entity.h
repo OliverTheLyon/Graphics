3a81415d82eb4910069dc072c23df12fd832dca0
oliver lyon <oll724@mail.usask.ca>
2026-06-13 19:23:26 -0600

    did not push this one by accient

--- Utility/entity.h
+++ Utility/entity.h
@@ -1,13 +1,55 @@
-//
-// Created by oliverlyon on 2026-06-13.
-//
 
-#ifndef GAME_ENTITY_H
-#define GAME_ENTITY_H
+#pragma once
+
+#include <glm/ext/vector_float3.hpp>
+#include <glm/glm.hpp>
 
 
 class entity {
+
+private:
+    std::shared_ptr<mesh> mesh; //MESH
+    glm::vec3 curr_pos;         //current centroid position
+
+public:
+    /**
+     * generic constructor
+     */
+    entity();
+
+    /**
+     *
+     * @param new_mesh the entities mesh
+     */
+    entity(std::shared_ptr<mesh> new_mesh);
+
+    /**
+     * placeholder for if entities have any memory ownership
+     */
+    ~entity(){};
+
+    /**
+     *
+     * @param position (x,y,z) float vector for the location of the centroid
+     */
+    void set_curr_position(glm::vec3 position);
+
+    /**
+     *
+     * @param new_mesh new mesh pointer to replace the old one
+     */
+    void set_mesh(std::shared_ptr<mesh> new_mesh);
+
+    /**
+     *
+     * @return returns a pointer to the 3 float vector holding the current position
+     */
+    glm::vec3* get_curr_position();
+
+    /**
+     * TODO: THROW EXCEPTION when no mesh in entity
+     * @return weak copy pointer to the mesh inside the entity
+     */
+    std::weak_ptr<mesh> get_mesh();
 };
 
-
-#endif //GAME_ENTITY_H
