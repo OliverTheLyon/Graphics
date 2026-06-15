600d37ea49cf2704cd50db55350bad6763d2ce18
oliver lyon <oll724@mail.usask.ca>
2026-06-14 20:51:23 -0600

    cleaned up logger, made it thread safe and faster, forces pass by referance of strings and eraly rule out in log if the log is not high enough in Debug. Lastly I add a string array for the debug level in message just make the code shorter and less repetative.

--- logger.hpp
+++ logger.hpp
@@ -2,16 +2,17 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <mutex>
 
 enum class debug_level { INFO=0, DEBUG=1,  WARN=2, ERROR=3};
 namespace OKengine {
 	class logger{
 
 	private:
-		std::string logfile;
-		debug_level detail;
 		std::ofstream outstream;
-
+		debug_level detail;
+		std::string logfile;
+		std::mutex log_mutex_;
 		
 		
 		logger(std::string path="program.log", debug_level dl=debug_level::INFO);
@@ -25,6 +26,6 @@ namespace OKengine {
 		};
 
 		void SetLevel(debug_level lvl);
-		void log(std::string msg, debug_level level);
+		void log(const std::string & msg, debug_level level);
 	};
 }
